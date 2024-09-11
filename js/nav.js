875e5387d6d3c89e25e824dccd3a3448fa125924
Nguyễn Minh Hoàng <sapbaohapcoc@gmail.com>
2024-09-12 01:31:42 +0700

    navbar adaptive

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+function loadNav() {
   fetch("/partials/nav.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("nav-placeholder").innerHTML = data;
-    });
-});
+    })
+    .catch((error) => console.error("Error loading navigation:", error));
+}
+
+function toggleMenu() {
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+  dropdownMenu.style.display =
+    dropdownMenu.style.display === "flex" ? "none" : "flex";
+}
+
+// Load the navigation bar when the page loads
+document.addEventListener("DOMContentLoaded", loadNav);
