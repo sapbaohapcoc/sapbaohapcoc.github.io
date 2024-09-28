2571b7db9ed82fbc14611447a17f716826735b0f
Nguyễn Minh Hoàng <macarong0986711447@gmail.com>
2024-09-29 01:27:27 +0700

    update wiki

--- coquanbachluyen/js/home.js
+++ coquanbachluyen/js/home.js
@@ -19,3 +19,28 @@ function toggleItem() {
   heroList.style.display = "none";
   weaponList.style.display = "none"
 }
+
+const imgs = document.querySelectorAll(".hero, .weapon, .item");
+
+imgs.forEach(img => {
+  const tooltip = img.nextElementSibling;
+
+  img.addEventListener("mousemove", (e) => {
+    tooltip.style.display = "flex";
+
+    const screenWidth = window.innerWidth,
+          tooltipWidth = 500,
+          spacetoRight = screenWidth - e.pageX;
+
+    if (spacetoRight < tooltipWidth) {
+      tooltip.style.right = `${screenWidth - e.pageX + 10}px`
+    } else {
+      tooltip.style.left = `${e.pageX + 10}px`;
+    }
+    tooltip.style.top = `${e.pageY - 67}px`;
+  });
+
+  img.addEventListener("mouseleave", () => {
+    tooltip.style.display = "none";
+  })
+})
